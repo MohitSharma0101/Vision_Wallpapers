2186aff0de6fafeb878dd3dcf195cf188e277a38
lalit0111 <tilwanil818@gmail.com>
2021-01-21 15:38:03 +0530

    view binding

--- app/src/main/java/com/vision/wallpapers/HomeFragment.kt
+++ app/src/main/java/com/vision/wallpapers/HomeFragment.kt
@@ -1,6 +1,36 @@
 package com.vision.wallpapers
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.vision.wallpapers.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    lateinit var binding: FragmentHomeBinding
+    lateinit var gridLayoutManager: GridLayoutManager
+    lateinit var adapter: Adapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        gridLayoutManager = GridLayoutManager(context, 2)
+        adapter = Adapter()
+
+        setupRecyclerView(binding.homeRecyclerView, gridLayoutManager, adapter)
+
+
+    }
+
+    private fun setupRecyclerView(
+        recyclerView: RecyclerView,
+        gridLayoutManager: GridLayoutManager,
+        adapter: Adapter
+    ) {
+        recyclerView.layoutManager = gridLayoutManager
+        recyclerView.adapter = adapter
+    }
 }
