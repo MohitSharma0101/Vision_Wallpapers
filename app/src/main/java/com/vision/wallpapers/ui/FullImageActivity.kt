c6c0ce8646808d2d340b3d45fc2dd7edc9f5a85d
lalit0111 <tilwanil818@gmail.com>
2021-01-29 16:42:55 +0530

    fix main activity bug.

--- app/src/main/java/com/vision/wallpapers/ui/FullImageActivity.kt
+++ app/src/main/java/com/vision/wallpapers/ui/FullImageActivity.kt
@@ -1,10 +1,8 @@
 package com.vision.wallpapers.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import coil.api.load
-import coil.transform.BlurTransformation
-import com.bumptech.glide.Glide
 import com.vision.wallpapers.databinding.ActivityFullImageBinding
 
 class FullImageActivity : AppCompatActivity() {
@@ -16,10 +14,7 @@ class FullImageActivity : AppCompatActivity() {
         binding = ActivityFullImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val url = intent.getStringExtra("url")
-       // binding.wallpaperIv.load(url)
-        Glide.with(this).load(url).fitCenter().into(binding.wallpaperIv)
-//        binding.backImage.load(url){
-//            transformations(BlurTransformation(this@FullImageActivity,5f))
-//        }
+        binding.wallpaperIv.load(url)
+//        Glide.with(this).load(url).centerCrop().into(binding.wallpaperIv)
     }
 }
