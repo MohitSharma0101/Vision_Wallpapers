d8d44bd87685d192ee299b3f841023e2a4b416a9
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2021-01-30 16:18:19 +0530

    saved section implemented

--- app/src/main/java/com/vision/wallpapers/Adapter.kt
+++ app/src/main/java/com/vision/wallpapers/Adapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.graphics.drawable.toDrawable
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -15,15 +16,21 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.vision.wallpapers.databinding.PictureCardBinding
 import com.vision.wallpapers.model.Response
+import com.vision.wallpapers.model.alphaCoder.AlphaPhotoResponseItem
 import com.vision.wallpapers.util.Palette
+import kotlinx.coroutines.launch
 
-class Adapter:RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(private val viewModel: WallpaperViewModel):RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     class ViewHolder(var binding: PictureCardBinding) : RecyclerView.ViewHolder(binding.root)
     private var onItemClickListener:( (View,String) -> Unit )? = null
+    private var onSaveClickListener:( (AlphaPhotoResponseItem) -> Unit )? = null
 
     fun setOnItemClickListener(listener: (View,String) -> Unit){
         onItemClickListener = listener
+    }
+    fun setSaveOnClickListener(listener: (AlphaPhotoResponseItem) -> Unit){
+        onSaveClickListener = listener
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -38,11 +45,30 @@ class Adapter:RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val photo = differ.currentList[position]
+        var isSaved = false
         holder.apply {
             binding.wallpaperIv.load(photo.urlImage){
                 crossfade(true)
                 allowHardware(true)
                 placeholder(Color.parseColor( Palette.LIGHT[position % Palette.LIGHT.size] ).toDrawable())
+            }
+            viewModel.viewModelScope.launch {
+                if(viewModel.isWallpaperSaved(photo as AlphaPhotoResponseItem)){
+                    binding.favBtn.setImageResource(R.drawable.ic_heart_filled)
+                    isSaved = true
+                }else{
+                    binding.favBtn.setImageResource(R.drawable.heart_white)
+                    isSaved = false
+                }
+            }
+
+            binding.favBtn.setOnClickListener {
+                if(!isSaved){
+                    viewModel.saveWallpaper(photo as AlphaPhotoResponseItem)
+                }else{
+                    viewModel.deleteWallpaper(photo as AlphaPhotoResponseItem)
+                }
+                notifyDataSetChanged()
             }
             binding.wallpaperIv.setOnClickListener {
                 onItemClickListener?.let {
--- app/src/main/java/com/vision/wallpapers/database/WallpaperDao.kt
+++ app/src/main/java/com/vision/wallpapers/database/WallpaperDao.kt
@@ -2,19 +2,23 @@ package com.vision.wallpapers.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.vision.wallpapers.model.alphaCoder.AlphaPhotoResponseItem
 import com.vision.wallpapers.model.pexels.Wallpaper
 
 @Dao
 interface WallpaperDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun save(wallpaper: Wallpaper):Long
+    suspend fun save(wallpaper: AlphaPhotoResponseItem):Long
 
-    @Query("SELECT * FROM wallpaper ORDER BY id DESC")
-    fun getAllArticles():LiveData<List<Wallpaper>>
+    @Query("SELECT * FROM wallpaper")
+    fun getAllWallpaper():LiveData<List<AlphaPhotoResponseItem>>
+
+    @Query("SELECT * FROM wallpaper WHERE id= :q")
+    suspend fun getWallpaperByTitle( q: Int):AlphaPhotoResponseItem
 
     @Delete
-    suspend fun deleteWallpaper(wallpaper: Wallpaper)
+    suspend fun deleteWallpaper(wallpaper: AlphaPhotoResponseItem)
 
     @Query("DELETE FROM wallpaper")
     suspend fun deleteAllWallpaper()
