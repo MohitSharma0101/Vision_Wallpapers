364f5a1f9e7e6bdd4e833f1f421c8bb2b5f573c2
lalit0111 <tilwanil818@gmail.com>
2021-01-31 16:27:57 +0530

    completed search and category search.

--- app/src/main/java/com/vision/wallpapers/ui/CategoriesFragment.kt
+++ app/src/main/java/com/vision/wallpapers/ui/CategoriesFragment.kt
@@ -1,13 +1,15 @@
 package com.vision.wallpapers.ui
 
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +25,7 @@ import com.vision.wallpapers.WallpaperViewModel
 import com.vision.wallpapers.databinding.FragmentCategoriesBinding
 import com.vision.wallpapers.util.Constants.Categories
 import com.vision.wallpapers.util.Constants.Colors
+import com.vision.wallpapers.util.Resources
 
 
 class CategoriesFragment:Fragment(R.layout.fragment_categories) {
@@ -50,12 +53,33 @@ class CategoriesFragment:Fragment(R.layout.fragment_categories) {
 
         adapter.differ.submitList(Colors)
 
+        showAdapter.setOnItemClickListener { image, url ->
+            val intent = Intent(context, FullImageActivity::class.java)
+            intent.putExtra("url", url)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                (activity as MainActivity),
+                image,
+                "photo"
+            )
+            val bundle = ActivityOptionsCompat.makeCustomAnimation(
+                requireContext(),
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            ).toBundle()
+            startActivity(intent, bundle)
+        }
+
         binding.searchBar.setOnEditorActionListener { textView, actionId, keyEvent ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 hideKeyboard()
-                viewModel.searchAlphaPhotos(binding.searchBar.text.toString())
-                Toast.makeText(context, binding.searchBar.text.toString(), Toast.LENGTH_SHORT)
-                    .show()
+                searchAlpha(textView.text.toString())
+                binding.recentCard.visibility = View.GONE
+                binding.recentText.visibility = View.GONE
+                binding.colorText.visibility = View.GONE
+                binding.colorRecyclerView.visibility = View.GONE
+                binding.categoriesText.visibility = View.GONE
+                binding.viewPager.visibility = View.GONE
+                binding.showRecyclerView.visibility = View.VISIBLE
 
                 return@setOnEditorActionListener true
             }
@@ -63,8 +87,16 @@ class CategoriesFragment:Fragment(R.layout.fragment_categories) {
         }
 
         adapter.setOnItemClickListener { name ->
-            viewModel.searchAlphaPhotos(name)
-            Toast.makeText(context, name, Toast.LENGTH_SHORT).show()
+            searchAlpha(name)
+            binding.searchBar.setText(name)
+            binding.recentCard.visibility = View.GONE
+            binding.recentText.visibility = View.GONE
+            binding.colorText.visibility = View.VISIBLE
+            binding.colorRecyclerView.visibility = View.VISIBLE
+            binding.categoriesText.visibility = View.GONE
+            binding.viewPager.visibility = View.GONE
+            binding.showRecyclerView.visibility = View.VISIBLE
+
         }
 
         binding.dropdown.setOnClickListener {
@@ -102,6 +134,19 @@ class CategoriesFragment:Fragment(R.layout.fragment_categories) {
             binding.chipGroup2.addView(chip)
         }
 
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (binding.categoriesText.visibility == View.GONE) {
+                binding.searchBar.setText("")
+                binding.showRecyclerView.visibility = View.GONE
+                binding.recentCard.visibility = View.VISIBLE
+                binding.recentText.visibility = View.VISIBLE
+                binding.colorText.visibility = View.VISIBLE
+                binding.colorRecyclerView.visibility = View.VISIBLE
+                binding.categoriesText.visibility = View.VISIBLE
+                binding.viewPager.visibility = View.VISIBLE
+            }
+        }
+
     }
 
     private fun setupColorsRecyclerView() {
@@ -133,8 +178,15 @@ class CategoriesFragment:Fragment(R.layout.fragment_categories) {
                 val text = view.findViewById<TextView>(R.id.categoryName)
                 text.text = Categories[position].name
                 view.setOnClickListener {
-                    viewModel.searchAlphaPhotos(Categories[position].name)
-                    Toast.makeText(context, Categories[position].name, Toast.LENGTH_SHORT).show()
+                    searchAlpha(Categories[position].name)
+                    binding.searchBar.setText(Categories[position].name)
+                    binding.recentCard.visibility = View.GONE
+                    binding.recentText.visibility = View.GONE
+                    binding.colorText.visibility = View.VISIBLE
+                    binding.colorRecyclerView.visibility = View.VISIBLE
+                    binding.categoriesText.visibility = View.GONE
+                    binding.viewPager.visibility = View.GONE
+                    binding.showRecyclerView.visibility = View.VISIBLE
                 }
             }
             show()
@@ -146,10 +198,25 @@ class CategoriesFragment:Fragment(R.layout.fragment_categories) {
     private fun hideKeyboard() {
         val view: View? = activity?.currentFocus
         if (view != null) {
-            val imm: InputMethodManager? = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+            val imm: InputMethodManager? =
+                context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
             imm?.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
+
+    private fun searchAlpha(query: String) {
+        viewModel.searchAlphaPhotos(query)
+        viewModel.alphaSearchPhotos.observe(viewLifecycleOwner, {
+            when (it) {
+                is Resources.Success -> {
+                    it.data?.let { list ->
+                        showAdapter.differ.submitList(list.wallpapers)
+                    }
+                }
+            }
+        })
+    }
+
 
 }
 
