ced33951bd0b1df815ced66f2588ad8b431e869d
lalit0111 <tilwanil818@gmail.com>
2021-02-03 18:45:28 +0530

    Created settings fragment.

--- app/src/main/java/com/vision/wallpapers/ui/SettingsFragment.kt
+++ app/src/main/java/com/vision/wallpapers/ui/SettingsFragment.kt
@@ -1,9 +1,20 @@
 package com.vision.wallpapers.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.vision.wallpapers.R
+import com.vision.wallpapers.databinding.FragmentSettingsBinding
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    lateinit var binding: FragmentSettingsBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSettingsBinding.bind(view)
 
 
+    }
 }
